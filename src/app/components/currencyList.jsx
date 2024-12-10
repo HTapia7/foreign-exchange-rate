b430e56d4ee1876ed348a48124a7c7ce7d4c0501
Hernan Tapia <tapiahernan5@gmail.com>
2024-12-09 23:19:28 -0800

    added api for USD

---
+++ src/app/components/currencyList.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { latestCurrency } from "../libs/currencyApi.js";
+
+const App = () => {
+  const [conversionRates, setConversionRates] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await latestCurrency();
+        setConversionRates(data.conversion_rates);
+      } catch (error) {
+        console.error("Error fetching conversion rates:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      {loading ? (
+        <h1>Loading conversion rates...</h1>
+      ) : conversionRates ? (
+        <div>
+          <h1>Conversion Rates:</h1>
+          <ul>
+            {Object.entries(conversionRates).map(([currency, rate]) => (
+              <li key={currency}>
+                {currency}: {rate}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <h1>Failed to load conversion rates.</h1>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/app/libs/currencyApi.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://v6.exchangerate-api.com/v6/";
+const API_KEY = process.env.NEXT_PUBLIC_EXCHANGE_RATE_API_KEY; 
+
+export async function latestCurrency() {
+  
+  try {
+    const response = await axios.get(`${BASE_URL}${API_KEY}/latest/USD`);
+    const data = response.data;
+    return data;
+    
+  } catch (error) {
+    console.error("Error fetching the latest currency rates:", error.message);
+  }
+}
